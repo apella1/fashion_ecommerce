0442b6fb5ec2718a36e87204c4ec77c0b99527fb
apella1 <johncollons5@gmail.com>
2023-03-28 15:42:36 +0300

    Configure redux

---
+++ src/app/store.ts
@@ -0,0 +1,5 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// creates a redux store and enables devtools that allows for inspection while developing
+
+export default configureStore({ reducer: {} })
