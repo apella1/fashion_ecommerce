6155c03b5936a4d6e07a4a5af1db2a9ce2303737
apella1 <johncollons5@gmail.com>
2023-03-29 18:01:57 +0300

    Add header links

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,6 +4,46 @@ const Home = () => {
   return (
     <section className="p-4 flex flex-col">
       <Header />
+      <section className="grid grid-cols-3 p-10 gap-6">
+        <section className="flex flex-col space-y-3 px-4">
+          <a href="/" className="flex items-center justify-between hover:text-blue-900">
+            <p>Supermarket</p>
+          </a>
+          <a href="/" className="flex items-center justify-between hover:text-blue-900">
+            <p>Phones</p>
+          </a>
+          <a href="/" className="flex items-center justify-between hover:text-blue-900">
+            <p>Sports Equipment</p>
+          </a>
+          <a href="/" className="flex items-center justify-between hover:text-blue-900">
+            <p>Fashion</p>
+          </a>
+          <a href="/" className="flex items-center justify-between hover:text-blue-900">
+            <p>Gaming</p>
+          </a>
+          <a href="/" className="flex items-center justify-between hover:text-blue-900">
+            <p>Baby Products</p>
+          </a>
+          <a href="/" className="flex items-center justify-between hover:text-blue-900">
+            <p>Appliances</p>
+          </a>
+          <a href="/" className="flex items-center justify-between hover:text-blue-900">
+            <p>Health and Beauty</p>
+          </a>
+          <a href="/" className="flex items-center justify-between hover:text-blue-900">
+            <p>Computing and TV</p>
+          </a>
+          <a href="/" className="flex items-center justify-between hover:text-blue-900">
+            <p>Other Categories</p>
+          </a>
+        </section>
+        <div>
+          <p>Sliding images go here</p>
+        </div>
+        <div>
+          <p>Other Section</p>
+        </div>
+      </section>
     </section>
   );
 };
