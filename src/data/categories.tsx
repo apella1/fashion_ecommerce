9a4ad6e8c072bd3d70b0690ae0910b57235bf769
apella1 <johncollons5@gmail.com>
2024-04-14 17:33:15 +0300

    refactor: add home categories template

---
+++ src/data/categories.tsx
@@ -0,0 +1,44 @@
+export const categories = [
+  {
+    name: "Supermarket",
+    link: "/supermarket",
+  },
+  {
+    name: "Phones",
+    link: "/phones",
+  },
+  {
+    name: "Sports Equipment",
+
+    link: "/sports-equipment",
+  },
+  {
+    name: "Fashion",
+
+    link: "/fashion",
+  },
+  {
+    name: "Game",
+    link: "/game",
+  },
+  {
+    name: "Baby Products",
+    link: "/baby-products",
+  },
+  {
+    name: "Appliances",
+    link: "/appliances",
+  },
+  {
+    name: "Health and Beauty",
+    link: "/health-and-beauty",
+  },
+  {
+    name: "Computing and TV",
+    link: "/computing",
+  },
+  {
+    name: "Other Categories",
+    link: "/other-categories",
+  },
+];
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import { Header } from "../../components";
+import { categories } from "../../data/categories";
 
 const Home = () => {
   return (
@@ -6,36 +7,14 @@ const Home = () => {
       <Header />
       <section className="grid grid-cols-3 p-10 gap-6">
         <section className="flex flex-col space-y-3 px-4">
-          <a href="/" className="flex items-center justify-between hover:text-blue-900">
-            <p>Supermarket</p>
-          </a>
-          <a href="/" className="flex items-center justify-between hover:text-blue-900">
-            <p>Phones</p>
-          </a>
-          <a href="/" className="flex items-center justify-between hover:text-blue-900">
-            <p>Sports Equipment</p>
-          </a>
-          <a href="/" className="flex items-center justify-between hover:text-blue-900">
-            <p>Fashion</p>
-          </a>
-          <a href="/" className="flex items-center justify-between hover:text-blue-900">
-            <p>Gaming</p>
-          </a>
-          <a href="/" className="flex items-center justify-between hover:text-blue-900">
-            <p>Baby Products</p>
-          </a>
-          <a href="/" className="flex items-center justify-between hover:text-blue-900">
-            <p>Appliances</p>
-          </a>
-          <a href="/" className="flex items-center justify-between hover:text-blue-900">
-            <p>Health and Beauty</p>
-          </a>
-          <a href="/" className="flex items-center justify-between hover:text-blue-900">
-            <p>Computing and TV</p>
-          </a>
-          <a href="/" className="flex items-center justify-between hover:text-blue-900">
-            <p>Other Categories</p>
-          </a>
+          {categories.map((category) => (
+            <a
+              href={category.link}
+              className="flex items-center justify-between hover:text-blue-900"
+            >
+              <p>{category.name}</p>
+            </a>
+          ))}
         </section>
         <div>
           <p>Sliding images go here</p>
