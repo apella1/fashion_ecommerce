761f10f3a87b859c9c33a14791441bb3046ab3ee
apella1 <johncollons5@gmail.com>
2024-04-22 20:32:44 +0300

    config: set up testing (react testing library && vitest)

---
+++ tests/components/Footer.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import Footer from "../../src/components/footer/Footer";
+
+describe("Footer", () => {
+  it("should render the footer with an heading", () => {
+    render(<Footer />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent("Footer");
+  });
+});
